3b146b7fa2ac10f046bfab8b53ce9feacf438a36
Michael Scheurer <michael.scheurer@seccom.ch>
2026-01-26 17:09:04 +0100

    fix env var naming

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -7,14 +7,9 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    clockodoApiEmail: process.env.CLOCKODO_API_EMAIL,
-    clockodoApiKey: process.env.CLOCKODO_API_KEY,
-    jiraEmail: process.env.NUXT_JIRA_API_EMAIL,
-    jiraApiKey: process.env.NUXT_JIRA_API_KEY,
-
     public: {
-      directusClientApiUrl: process.env.NUXT_DIRECTUS_CLIENT_API_URL,
-      directusServerApiUrl: process.env.NUXT_DIRECTUS_SERVER_API_URL,
+      directusClientApiUrl: process.env.DIRECTUS_CLIENT_API_URL,
+      directusServerApiUrl: process.env.DIRECTUS_SERVER_API_URL,
     }
   },
 
