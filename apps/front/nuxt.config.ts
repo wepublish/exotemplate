2a0a7a732339036c4e49859e3129f1558a6be613
Elias Summermatter <elias.summermatter@seccom.ch>
2026-01-26 09:37:01 +0100

    Add pipeline

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   routeRules: {
-    '/': { prerender: true }
+    '/': { prerender: false }
   },
 
   compatibilityDate: '2025-01-15',
