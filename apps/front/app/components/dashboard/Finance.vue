56cdda187399c1d4bba5e201f29a694101140b72
Michael Scheurer <michael.scheurer@seccom.ch>
2026-03-11 14:51:11 +0100

    adapt to api

--- apps/front/app/components/dashboard/Finance.vue
+++ apps/front/app/components/dashboard/Finance.vue
@@ -30,7 +30,7 @@
   )
 
   const progressColor = computed<string>(() => {
-    const sums = props.sums?.totalUsedPercentage || 100
+    const sums = props.sums?.totalUsedPercentage || 0
     if (sums >= 90) {
       return 'error'
     }
@@ -75,7 +75,7 @@
 
         <div>
           <UProgress
-            :model-value="sums.totalUsedPercentage || 100"
+            :model-value="sums.totalUsedPercentage"
             status
             size="2xl"
             class="col-span-6"
@@ -83,7 +83,7 @@
           >
             <template #status>
               <p :class="`text-${progressColor}`">
-                {{ sums.totalUsedPercentage || 100 }} %
+                {{ sums.totalUsedPercentage }} %
               </p>
             </template>
           </UProgress>
